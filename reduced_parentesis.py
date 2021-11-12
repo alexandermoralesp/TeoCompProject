35b1686c31d54212a5c7041f2db9cde31e06d099
Rodrigo Salazar <rodrigo.salazar@utec.edu.pe>
2021-11-12 16:12:10 -0500

    Reduced parentesis

---
+++ reduced_parentesis.py
@@ -0,0 +1,112 @@
+
+class Automata:
+    def __init__(self, n, start_state, end_state_list):
+                
+        # Create Adjacency Matrix
+        self.adjacency_matrix = [[None for i in range(n+2)] for j in range(n+2)]
+        
+        # Initalize normalized state
+        # Normalized Start: n
+        # Normalized End: n+1
+        normalized_start_index = n
+        normalized_end_index = n+1
+
+        self.adjacency_matrix[normalized_start_index][start_state] = "" # Epsilon
+        for state in end_state_list:
+            self.adjacency_matrix[state][normalized_end_index] = "" # Epsilon transition
+        
+
+    def add_transition(self, state, transition, next): # Método para añadir un estado al automata
+        if self.adjacency_matrix[state][next] is None: 
+            self.adjacency_matrix[state][next] = str(transition)
+        else:
+            # Completado: NORMALIZACION de doble transicion
+            self.adjacency_matrix[state][next] = str(transition)+"+"+self.adjacency_matrix[state][next]
+
+    def display(self):
+        print(end="\t")
+        for j in range( len(self.adjacency_matrix[0]) ):
+            print( j, end="\t")
+        print()
+
+        for i in range( len(self.adjacency_matrix) ):
+            print(i, end = "\t")
+            for j in range( len(self.adjacency_matrix[0]) ):
+                print( self.adjacency_matrix[i][j], end="\t")
+            print()
+    
+    def get_regular_expression(self):
+        n = len(self.adjacency_matrix)
+
+        for s in range( n ):
+            # s = estado a remover
+
+            # Declarando elemento de loop (*)
+            if self.adjacency_matrix[s][s] is None:
+                loop = ""
+            elif len(self.adjacency_matrix[s][s]) == 1 or self.single_parentesis(self.adjacency_matrix[s][s]):
+                loop = str(self.adjacency_matrix[s][s]) + "*"
+            else:
+                loop = "("+str(self.adjacency_matrix[s][s])+")*"
+
+            # Lista de todas las entras y salidas validas
+            in_transitions = [i for i in range(s+1,n) if self.adjacency_matrix[i][s] is not None]
+            out_transitions = [o for o in range(s+1,n) if self.adjacency_matrix[s][o] is not None]
+
+            # Combinacion de todas las entradas validas con todas las salidas valdias
+            for i in in_transitions:
+                for o in out_transitions:
+                    if self.adjacency_matrix[i][o] is None:
+                        self.adjacency_matrix[i][o] = self.adjacency_matrix[i][s] + loop +  self.adjacency_matrix[s][o] 
+                    else:
+                        self.adjacency_matrix[i][o] = "(" + self.adjacency_matrix[i][o] + "+" + self.adjacency_matrix[i][s] + loop + self.adjacency_matrix[s][o] + ")" 
+            
+        return self.adjacency_matrix[n-2][n-1]
+
+    def single_parentesis(self, regex):
+    
+        if regex[0] != "(" or regex[-1] != ")":
+            return False
+
+        parentesis_counter = 1
+        for c in regex[1:-1]:
+            if c == "(":
+                parentesis_counter += 1
+            elif c == ")":
+                parentesis_counter -= 1
+            
+            if parentesis_counter <= 0:
+                return False
+        return True
+
+
+def main():
+    line = input().split() # Linea de ingreso de valores de primera fila
+    n = int(line[0]) # Definir cuantos estados habrán
+    start = int(line[1]) # Definir estado inicial
+    end_list = [int(line[i+3]) for i in range( int(line[2]) )] # Dar los ids de los estados finales
+
+    if end_list: # Si existen estados finales
+
+        automata = Automata(n, start, end_list) # Creamos la clase automata 
+
+        for i in range(2*n ): # Bucle para las siguientes 2n filas
+            line = input().split() # Fila de transicion estilo  "estado" "entrada" "siguiente estado"
+            state = int(line[0]) # Extracción del estado
+            transition = str(line[1]) # Extracción del caracter ingresado
+            next = int(line[2]) # Extraccion del siguiente estado
+            automata.add_transition(state, transition, next) # Se añade la transicion al automata
+        
+        regex = automata.get_regular_expression()
+    else:
+        # COMPLETADO: CASO ESQUINA: Cuando le automata no tiene estados finales se aceptan inputs pero se sabe que el regex va a ser nulo
+        for i in range(2*n ):
+            line = input()
+        regex = None
+
+    print(regex) # Se imprime la expresión regular
+
+
+main()
+
+
